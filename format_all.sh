8f423478ebdc0e0fe2296b7e14954bba7930cb68
Robert Janca <robert@indoo.rs>
2015-10-09 17:08:12 +0200

    Fixed formatting for testcases

--- format_all.sh
+++ format_all.sh
@@ -1,4 +1,4 @@
-for DIRECTORY in source include
+for DIRECTORY in source include test
 do
 	echo "Formatting code in $DIRECTORY/"
 	find "$DIRECTORY" \( -name '*.h' -or -name '*.cpp' \) -print0 | xargs -0 clang-format -i
--- test/Main.cpp
+++ test/Main.cpp
@@ -8,123 +8,125 @@
 /**
 * \brief Assembles source file and stores bytecode in output file.
 */
-bool assemble(kern::IAssembler& assembler, const std::string& asmSource, const std::string& byteCode)
+bool assemble(kern::IAssembler &assembler, const std::string &asmSource,
+              const std::string &byteCode)
 {
-	// Open streams
-	std::ifstream ifs(asmSource);
-	if (!ifs.is_open())
-	{
-		std::cout << "Failed to open the input file " << asmSource << std::endl;
-		return false;
-	}
+  // Open streams
+  std::ifstream ifs(asmSource);
+  if (!ifs.is_open())
+  {
+    std::cout << "Failed to open the input file " << asmSource << std::endl;
+    return false;
+  }
 
-	std::ofstream ofs(byteCode, std::ios::binary);
-	if (!ofs.is_open())
-	{
-		std::cout << "Failed to open the output file " << byteCode << std::endl;
-		return false;
-	}
+  std::ofstream ofs(byteCode, std::ios::binary);
+  if (!ofs.is_open())
+  {
+    std::cout << "Failed to open the output file " << byteCode << std::endl;
+    return false;
+  }
 
-	// Assemble input into output stream
-	if (!assembler.assemble(ifs, ofs))
-	{
-		std::cout << "Failed to assemble the input file " << asmSource << std::endl;
-		return false;
-	}
-	return true;
+  // Assemble input into output stream
+  if (!assembler.assemble(ifs, ofs))
+  {
+    std::cout << "Failed to assemble the input file " << asmSource << std::endl;
+    return false;
+  }
+  return true;
 }
 
 /**
 * \brief Executes main function of bytecode script.
 */
-bool execute(kern::IVirtualMachine& vm, const std::string& byteCode)
+bool execute(kern::IVirtualMachine &vm, const std::string &byteCode)
 {
-	std::ifstream ifs(byteCode, std::ios::binary);
-	if (!ifs.is_open())
-	{
-		std::cout << "Failed to open the input file " << byteCode << std::endl;
-		return false;
-	}
-	if (!vm.load(ifs))
-	{
-		std::cout << "Failed to load the input file " << byteCode << std::endl;
-		return false;
-	}
-	if (!vm.callFunction("main"))
-	{
-		std::cout << "Failed to execute the file " << byteCode << std::endl;
-		return false;
-	}
-	return true;
+  std::ifstream ifs(byteCode, std::ios::binary);
+  if (!ifs.is_open())
+  {
+    std::cout << "Failed to open the input file " << byteCode << std::endl;
+    return false;
+  }
+  if (!vm.load(ifs))
+  {
+    std::cout << "Failed to load the input file " << byteCode << std::endl;
+    return false;
+  }
+  if (!vm.callFunction("main"))
+  {
+    std::cout << "Failed to execute the file " << byteCode << std::endl;
+    return false;
+  }
+  return true;
 }
 
 /**
 * \brief Assembles and executes assembly script.
 */
-bool testAsm(kern::IAssembler& assembler, kern::IVirtualMachine& vm, const std::string& asmCode, const std::string& byteCode)
+bool testAsm(kern::IAssembler &assembler, kern::IVirtualMachine &vm,
+             const std::string &asmCode, const std::string &byteCode)
 {
-	if (!assemble(assembler, asmCode, byteCode))
-	{
-		std::cout << "Assembly failed" << std::endl;
-		return false;
-	}
+  if (!assemble(assembler, asmCode, byteCode))
+  {
+    std::cout << "Assembly failed" << std::endl;
+    return false;
+  }
 
-	if (!execute(vm, byteCode))
-	{
-		std::cout << "Execute failed" << std::endl;
-		return false;
-	}
-	return true;
+  if (!execute(vm, byteCode))
+  {
+    std::cout << "Execute failed" << std::endl;
+    return false;
+  }
+  return true;
 }
 
 /**
 * \brief Starts testcase by name.
 */
-bool testAsmUtil(kern::IAssembler& assembler, kern::IVirtualMachine& vm, const std::string& testName)
+bool testAsmUtil(kern::IAssembler &assembler, kern::IVirtualMachine &vm,
+                 const std::string &testName)
 {
-	vm.clearScripts();
-	std::cout << std::endl << "--- Starting test case: " << testName << std::endl;
-	
-	bool ok = true;
-	if (!testAsm(assembler, vm, "test_data/testasm/" + testName + ".kasm", "test_data/testasm_out/" + testName + ".kbyte"))
-	{
-		ok = false;
-	}
-	std::cout << "Test case: testasm '" << testName << "' ";
-	if (ok)
-	{
-		std::cout << "SUCCEEDED" << std::endl;
-	}
-	else
-	{
-		std::cout << "FAILED" << std::endl;
-	}
-	return true;
+  vm.clearScripts();
+  std::cout << std::endl
+            << "--- Starting test case: " << testName << std::endl;
+
+  bool ok = true;
+  if (!testAsm(assembler, vm, "test_data/testasm/" + testName + ".kasm",
+               "test_data/testasm_out/" + testName + ".kbyte"))
+  {
+    ok = false;
+  }
+  std::cout << "Test case: testasm '" << testName << "' ";
+  if (ok)
+  {
+    std::cout << "SUCCEEDED" << std::endl;
+  }
+  else
+  {
+    std::cout << "FAILED" << std::endl;
+  }
+  return true;
 }
 
 /**
 * \brief Utility print function to be callable from script.
 */
-void print(std::string text)
+void print(std::string text) { std::cout << text << std::endl; }
+
+int main(int argc, char **argv)
 {
-	std::cout << text << std::endl;
-}
+  std::unique_ptr<kern::ICompiler> comp(kern::createCompiler());
+  std::unique_ptr<kern::IAssembler> assembler(kern::createAssembler());
+  std::unique_ptr<kern::IVirtualMachine> vm(kern::createVirtualMachine());
 
-int main(int argc, char** argv)
-{
-	std::unique_ptr<kern::ICompiler> comp(kern::createCompiler());
-	std::unique_ptr<kern::IAssembler> assembler(kern::createAssembler());
-	std::unique_ptr<kern::IVirtualMachine> vm(kern::createVirtualMachine());
+  // Utility print function
+  vm->addFunction("print", kern::createExternFunction(print));
 
-	// Utility print function
-	vm->addFunction("print", kern::createExternFunction(print));
+  testAsmUtil(*assembler, *vm, "test1");
+  testAsmUtil(*assembler, *vm, "test2");
+  testAsmUtil(*assembler, *vm, "test3");
+  testAsmUtil(*assembler, *vm, "test4");
+  testAsmUtil(*assembler, *vm, "test5");
+  testAsmUtil(*assembler, *vm, "test6");
 
-	testAsmUtil(*assembler, *vm, "test1");
-	testAsmUtil(*assembler, *vm, "test2");
-	testAsmUtil(*assembler, *vm, "test3");
-	testAsmUtil(*assembler, *vm, "test4");
-	testAsmUtil(*assembler, *vm, "test5");
-	testAsmUtil(*assembler, *vm, "test6");
-
-	return 0;
+  return 0;
 }
